49ea7ba6ea6627420e23f1d2f39def16db1763f6
sam <sam@localhost.localdomain>
2022-07-25 09:06:14 +0530

    selection sort initiated

--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,16 +1,19 @@
 // here I shall try out the selection sort today.
 // let us see how that goes
-
 const selectionSort = (arr) => {
-    // here we shall be sorting the array
-    // the array shall be sorted in an ascending order
-    let smallest = arr[0];
-    for (let i = 0; i <arr.length; i++) {
-        if (arr[i] < smallest) {
-            smallest = arr[i]
+    let start = 0;
+    let end = arr.length - 1;
+    while (start < end) {
+        for (let i = start; i < arr.length; i++) {
+            if (arr[i] < arr[start]) {
+                let tmp;
+                tmp = arr[i]; 
+                arr[i] = arr[start];
+                arr[start] = tmp;
+            }
         }
+        start++;
     }
-    return smallest; 
+    return arr;
 }
-
-console.log(selectionSort([4,5,3,1,2]))
+console.log(selectionSort([4,5,3,1,2]));
