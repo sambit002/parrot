28485a830bf8085e9c49ee4e4693ff00b11308df
sambit002 <sambit5150@gmail.com>
2023-07-26 10:16:09 +0530

    added plain js codes

---
+++ vanilla/vanilla.js
@@ -0,0 +1,5 @@
+//here we shall be writing vanilla vs code
+// An object can be created by a constructor function, which contains properties and function declarations
+// this keyword is used to bind the properties and functions to object instances.
+
+console.log("This is going to be fun");
