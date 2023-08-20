c3b05f933addaa61798dec8f54c2ba81e0bd7003
sambit002 <sambit5150@gmail.com>
2023-08-20 22:54:48 +0530

    primality code modified

--- revision/prime.js
+++ revision/prime.js
@@ -6,7 +6,7 @@ const prime = (n) => {
     if (n < 2) {
         return false;
     }
-    for (let i=2; i < n; i++) {
+    for (let i=2; i < Math.sqrt(n); i++) {
         if (n % i === 0) {
             return false
         }
